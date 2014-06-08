e1ec5d4b13a47ad9b3566da8e43d608f04f200ba
CHIKURA Shinsaku <shinsaku@chikura.me>
2014-06-08 21:13:59 +0900

    Add Hall interface.

---
+++ lib/hall.lua
@@ -0,0 +1,24 @@
+
+Hall = {}
+Hall.new = function(token, title, picture_url)
+  local this = {}
+  this.token = token
+  this.title = title
+  this.picture_url = picture_url
+  this.post_url = 'https://hall.com/api/1/services/generic/'..token
+
+  this.post = function(self, params)
+    local response = http.request {
+      method = 'POST',
+      url = self.post_url,
+      data = {
+        title = self.title,
+        message = params.heading .. '\n' .. params.message,
+        picture = self.picture_url
+      }
+    }
+    return response
+  end
+
+  return this
+end
