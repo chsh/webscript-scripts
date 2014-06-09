b31b95264fe3ec5473afe94103bfd7d497f064fb
CHIKURA Shinsaku <shinsaku@chikura.me>
2014-06-09 11:34:08 +0900

    Add html related functions.

---
+++ lib/view/html.lua
@@ -0,0 +1,18 @@
+-- lua module to generate hmtl tags
+
+
+local function link(text, url)
+  return '< a href="' .. url .. '">' .. text .. '</a>'
+end
+local function tag(tag, string)
+  return '<' .. tag .. '>' .. string .. '</' .. tag .. '>'
+end
+local function strong(string)
+	return tag('strong', string)
+end
+
+return {
+  link = link,
+  tag = tag,
+  strong = strong
+}
